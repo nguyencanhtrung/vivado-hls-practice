97c09f1c7ae634623dc3a4548f6328792be2dfb4
Trung C. Nguyen <nguyencanhtrung@me.com>
2016-09-09 18:54:46 +0200

    add debug

---
+++ testing/axi_stream_testing_hls/unusual_s2mm_vivado/debug/initialize_ps.tcl
@@ -0,0 +1,6 @@
+## initialize PS
+source /home/trungnguyen/Desktop/GitHub/vivado_hls/testing/axi_stream_testing_hls/unusual_s2mm_vivado/project_1/project_1.sdk/unusual_s2mm_sys_wrapper_hw_platform_0/ps7_init.tcl
+ps7_init
+ps7_post_config
+
+
